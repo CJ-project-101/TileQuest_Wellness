0d26a7c27fa5c5a4249e2651a830e6c132577ae3
Calvin K J <calvinkj412@gmail.com>
2025-10-28 00:20:13 +0530

    feat: Player Jump and Jump config

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -3,13 +3,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMoment : MonoBehaviour
 {
-    [SerializeField] float runSpeed =10f;
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 10f;
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
     
     void Start() 
     {
-        myRigidbody=GetComponent<Rigidbody2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
     
 
@@ -17,18 +22,45 @@ public class PlayerMoment : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
-        moveInput=value.Get<Vector2>();
+        moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+    
+    void OnJump(InputValue value)
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        
+        if(value.isPressed )
+        {
+            myRigidbody.linearVelocity += new Vector2(0f, jumpSpeed);
+        }
     }
 
     void Run()
     {
-        Vector2 playerVelocity=new Vector2(moveInput.x*runSpeed,myRigidbody.linearVelocity.y);
-        myRigidbody.linearVelocity= playerVelocity;
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.linearVelocity.y);
+        myRigidbody.linearVelocity = playerVelocity;
 
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("IsRunning", playerHasHorizontalSpeed);
+
+    }
+    
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.linearVelocity.x), 1f);
+        }
     }
 }
